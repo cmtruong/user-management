3d3d803abc4c139f130719982bfb3532ba26dc06
cmtruong <cong-minh.truong@capgemini.com>
2016-11-23 15:25:12 +0100

    Config hibernate

--- src/main/java/truong/capgemini/usermanagement/config/AppConfig.java
+++ src/main/java/truong/capgemini/usermanagement/config/AppConfig.java
@@ -1,12 +1,50 @@
 package truong.capgemini.usermanagement.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("truong.capgemini.usermanagement")
+@EnableTransactionManagement
+@PropertySource("classpath:hibernate-config.properties")
 public class AppConfig {
 
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		return messageSource;
+	}
+
+	@Bean(name = "viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("ds.database-driver"));
+		dataSource.setUrl(environment.getProperty("ds.url"));
+		dataSource.setUsername(environment.getProperty("ds.username"));
+		dataSource.setPassword(environment.getProperty("ds.password"));
+		return dataSource;
+	}
 }
